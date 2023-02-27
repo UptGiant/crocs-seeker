b6b081d2c99a9d4276a0f29917579b3bd7158545
UptGiant <jorgemartinez12335@gmail.com>
2023-02-26 20:57:55 -0600

    login and Register

---
+++ seeker/src/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmint = (sub) => {
+        sub.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form">
+            <form onSubmit={handleSubmint}>
+                <label htmlFor="email">email</label>
+                <input value={email} onChange={(sub) => setEmail(sub.target.value)} placeholder="abc@gmail.com" type="email" id="email" name="email"/>
+                <label htmlFor="password">password</label>
+                <input value={password} onChange={(sub) => setPassword(sub.target.value)} type="password" id="password" name="password"/>
+                <button>Login</button>
+            </form>
+            <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Regester</button>
+        </div>
+    )
+}
---
+++ seeker/src/Register.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+export const Register = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUserName] = useState('');
+
+    const handleSubmint = (sub) => {
+        sub.preventDefault();
+        console.log(email);
+    }
+    return (
+        <div className="auth-form">
+            <form onSubmit={handleSubmint}>
+                <label htmlFor="username">username</label>
+                <input value={username} onChange={(sub) => setUserName(sub.target.value)} type="username" id="username" name="username"/>
+                <label htmlFor="username">username</label>
+                <input value={email} onChange={(sub) => setEmail(sub.target.value)} type="email" id="email" name="email"/>
+                <label htmlFor="password">password</label>
+                <input value={password} onChange={(sub) => setPassword(sub.target.value)} type="password" id="password" name="password"/>
+                <button>Login</button>
+            </form>
+            <button onClick={() => props.onFormSwitch('login')}>Have an account already? Login</button>
+        </div>
+    )
+}
